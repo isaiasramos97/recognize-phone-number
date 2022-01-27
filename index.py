98799c841b8f850d634fb7f32b51bd48e0be173a
Isaias <isaiasramos_@outlook.com>
2022-01-27 17:50:57 -0300

    teste inicial
    Testando inicialmente com o formato padrão brasileiro

---
+++ index.py
@@ -0,0 +1,26 @@
+def is_phone_number(text):
+    if len(text) != 15:
+        return False
+    if text[0] != '(' or text[3] != ')':
+        return False
+    for i in range(1, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[4] != ' ':
+        return False
+    for i in range(5, 10):
+        if not text[i].isdecimal():
+            return False
+        if text[10] != '-':
+            return False
+    for i in range(11, 15):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+
+# (21) 98546-8979
+print('415-555-4242 is a phone number:')
+print(is_phone_number('415-555-4242'))
+print('(21) 96560-2272 is a phone number:')
+print(is_phone_number('(21) 96560-2272'))
